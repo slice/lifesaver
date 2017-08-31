03827664f0a7c3161def5211cc85e2d7f97cac3b
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-08-31 00:18:09 -0700

    Ignore bots by default

--- lifesaver/bot/bot.py
+++ lifesaver/bot/bot.py
@@ -15,11 +15,14 @@ class BotBase(commands.bot.BotBase):
 
     You should not be inheriting/using this class directly.
     """
-    def __init__(self, *args, extensions_path, **kwargs):
+    def __init__(self, *args, extensions_path: str, ignore_bots: bool = True, **kwargs):
         super().__init__(*args, **kwargs)
 
         #: The path from which to load extensions from, relative to the current directory.
         self.extensions_path = extensions_path
+
+        #: Specifies whether to ignore bots or not.
+        self.ignore_bots = ignore_bots
 
         #: A list of extensions names to reload when calling Bot.load_all().
         self._extension_load_list = []
@@ -51,6 +54,11 @@ class BotBase(commands.bot.BotBase):
             self.load_extension(extension_name)
 
         self.dispatch('lf_bot_load_all', unload_first)
+
+    async def on_message(self, message: discord.Message):
+        # Ignore bots if applicable.
+        if self.ignore_bots and message.author.bot:
+            return
 
 
 class Bot(BotBase, discord.Client):
