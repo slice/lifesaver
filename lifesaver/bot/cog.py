f4df04b23b658aaa9fff8fef20e8ff002d7e4c79
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-08-30 23:51:02 -0700

    Add a logger to every cog

--- lifesaver/bot/cog.py
+++ lifesaver/bot/cog.py
@@ -1,6 +1,12 @@
+import logging
+
+
 class Cog:
     """A base class for cogs."""
 
     def __init__(self, bot):
         #: The bot instance.
         self.bot = bot
+
+        #: The logger for this cog.
+        self.log = logging.getLogger('cog.' + type(self).__name__.lower())
