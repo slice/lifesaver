988152b7b65427224545bffd4cd7b635eebc03c1
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-09-10 20:18:09 -0700

    add truncation formatting util

--- lifesaver/utils/formatting.py
+++ lifesaver/utils/formatting.py
@@ -10,3 +10,20 @@ def codeblock(code: str, *, lang: str = '') -> str:
     :rtype: str
     """
     return "```{}\n{}\n```".format(lang, code)
+
+
+def truncate(text: str, desired_length: int) -> str:
+    """
+    Truncates text.
+
+    :param text: The text to truncate.
+    :type text: str
+    :param desired_length: The desired length.
+    :type desired_length: int
+    :return: The truncated text.
+    :rtype: str
+    """
+    if len(text) > desired_length:
+        return text[:desired_length-3] + '...'
+    else:
+        return text
