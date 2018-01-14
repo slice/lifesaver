25bbe243e659de08d07aa12ca314488187f38545
slice <ryaneft@gmail.com>
2018-01-14 10:19:42 -0800

    add license headers of doom

--- lifesaver/config.py
+++ lifesaver/config.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+MIT License
+
+Copyright (c) 2017 - 2018 slice
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from typing import Any, Dict
 
 from ruamel.yaml import YAML
@@ -48,7 +73,7 @@ class Config:
             self.__dict__.update(data)
 
     @classmethod
-    def load(cls, file: str) -> 'Config':
+    def load(cls, file: str):
         """
         Creates a new :class:`Config` and loads a YAML file into it.
 
