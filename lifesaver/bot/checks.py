8ee601d75853db8302b9f0ebc47284a23c943beb
slice <ryaneft@gmail.com>
2018-05-08 22:51:45 -0700

    Clean up is_nsfw_or_dm check

--- lifesaver/bot/checks.py
+++ lifesaver/bot/checks.py
@@ -32,7 +32,7 @@ from lifesaver.bot import Context
 def is_nsfw_or_dm():
     """A check that passes when the current command context is in a NSFW channel, or a DM."""
 
-    def _check(context: Context):
-        return (not context.guild) or (isinstance(context.channel, discord.TextChannel) and context.channel.nsfw)
+    def predicate(ctx: Context):
+        return not ctx.guild or (isinstance(ctx.channel, discord.TextChannel) and ctx.channel.nsfw)
 
-    return check(_check)
+    return check(predicate)
