2dedc44a5a83d7cea3537eb51e8fdeb099e9ac09
slice <ryaneft@gmail.com>
2018-05-08 22:34:56 -0700

    Expose BotConfig in lifesaver.bot

--- lifesaver/bot/__init__.py
+++ lifesaver/bot/__init__.py
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from .bot import Bot
+from .bot import Bot, BotConfig
 from .cog import Cog
 from .command import Command, Group, group, command
 from .context import Context
