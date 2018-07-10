51eb519599936df857e6fc9e0001dd0224514b4e
slice <ryaneft@gmail.com>
2018-07-10 11:04:11 -0700

    Allow for a custom Bot class when using cli

--- lifesaver/cli.py
+++ lifesaver/cli.py
@@ -1,8 +1,9 @@
 import asyncio
+import importlib
 import logging
 
 import click
-from lifesaver.bot import Bot
+from lifesaver.bot import Bot, BotConfig
 from lifesaver.logging import setup_logging
 
 
@@ -19,7 +20,22 @@ def cli(config):
     except ImportError:
         log.warn('uvloop not found, not using')
 
-    bot = Bot.with_config(config)
+    config = BotConfig.load(config)
+    custom_bot_module = getattr(config, 'bot_class')
+    bot_class = Bot
+
+    if custom_bot_module is not None:
+        module, class_name = custom_bot_module.split(':')
+        imported = importlib.import_module(module)
+        bot_class = getattr(imported, class_name)
+
+        if not issubclass(bot_class, Bot):
+            raise TypeError('Custom bot class is not a subclass of lifesaver.bot.Bot')
+
+        log.debug('Using custom bot class: %s', bot_class)
+
+    log.info('Booting bot.')
+    bot = bot_class.with_config_instance(config)
     bot.load_all()
     bot.run()
 
