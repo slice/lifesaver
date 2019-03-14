d22e57bda86d8be9d2d9347def030efd27ff3d2a
slice <ryaneft@gmail.com>
2019-03-13 17:20:25 -0700

    Fix history_reducer not adding to sets correctly

--- lifesaver/utils/messages.py
+++ lifesaver/utils/messages.py
@@ -5,9 +5,16 @@ from typing import Callable, Any, Type, Union, List, Set
 import discord
 
 
-async def history_reducer(ctx, reducer: Callable[[discord.Message], Any], *, ignore_duplicates=False,
-                          result_container_type: Type = list, **kwargs) -> Union[List, Set]:
-    """Iterate through message history and output a list of items determined by a function that receives each message.
+async def history_reducer(
+    ctx,
+    reducer: Callable[[discord.Message], Any],
+    *,
+    ignore_duplicates: bool = False,
+    result_container_type: Type = list,
+    **kwargs,
+) -> Union[List, Set]:
+    """Iterate through message history and output a list of items built from
+    a function that receives each message.
 
     Parameters
     ----------
@@ -21,32 +28,28 @@ async def history_reducer(ctx, reducer: Callable[[discord.Message], Any], *, ign
         Specifies the type of result container. Should be either ``list`` or ``set``.
     kwargs
         The kwargs to pass to the ``ctx.history`` method.
+
     Returns
     -------
         The list of items.
     """
-    if 'limit' not in kwargs:
-        raise TypeError('limit required')
-
-    history: List[discord.Message] = await ctx.history(limit=kwargs['limit']).flatten()
     results: Union[List, Set] = result_container_type()
 
-    for message in history:
+    async for message in ctx.history(**kwargs):
         result = await discord.utils.maybe_coroutine(reducer, message)
 
-        if result:
-            if ignore_duplicates and result in results:
-                continue
+        if not result or (ignore_duplicates and result in results):
+            continue
 
-            if isinstance(results, list):
-                if isinstance(result, list):
-                    results.extend(result)
-                else:
-                    results.append(result)
-            elif isinstance(results, set):
-                if isinstance(result, set):
-                    results = results | result
-                else:
-                    results.add(results)
+        if isinstance(results, list):
+            if isinstance(result, list):
+                results.extend(result)
+            else:
+                results.append(result)
+        elif isinstance(results, set):
+            if isinstance(result, set):
+                results = results | result
+            else:
+                results.add(result)
 
     return results
