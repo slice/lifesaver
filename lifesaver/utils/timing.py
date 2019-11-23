905147c08a95fd9f310d8162adf7dacb6b261da4
slice <ryaneft@gmail.com>
2019-11-23 10:47:19 -0800

    Manually implement lifesaver.utils.Ratelimiter
    This is so that we don't have to rely on discord.py internals.
    
    The semantics have also changed. You should use `Ratelimiter.hit` to
    count requests and return whether the token is being rate limited. To
    passively check if the token is being ratelimited without counting
    towards the request count, use `Ratelimiter.is_being_rate_limited`.
    
    Otherwise, the behavior should be identical.

--- lifesaver/utils/timing.py
+++ lifesaver/utils/timing.py
@@ -5,7 +5,7 @@
 """
 MIT License
 
-Copyright (c) 2018 slice, Rapptz
+Copyright (c) 2018 slice
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,24 +26,27 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-__all__ = ['Timer', 'format_seconds', 'Ratelimiter']
+__all__ = ["Timer", "format_seconds", "Ratelimiter"]
 
-from time import monotonic
+import time
+import typing as T
 
 from discord.ext import commands
 
 
 def format_seconds(seconds: int) -> str:
+    """Format a number of seconds into a readable string."""
+
     s = seconds
     ms = seconds * 1000
-    mcs = seconds * 1000000
+    mcs = seconds * 1_000_000
 
     if ms > 1000:
-        return f'{s:.2f}s'
+        return f"{s:.2f}s"
     elif ms < 1:
-        return f'{mcs:.2f}μs'
+        return f"{mcs:.2f}μs"
     else:
-        return f'{ms:.2f}ms'
+        return f"{ms:.2f}ms"
 
 
 class Timer:
@@ -54,11 +57,11 @@ class Timer:
         self.end: int = None
 
     def __enter__(self):
-        self.begin = monotonic()
+        self.begin = time.monotonic()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.end = monotonic()
+        self.end = time.monotonic()
 
     @property
     def duration(self):
@@ -75,36 +78,77 @@ class Timer:
     @property
     def microseconds(self):
         """Return the duration that this timer ran, in microseconds."""
-        return self.duration * 1000000
+        return self.duration * 1_000_000
 
     def __str__(self):
         return format_seconds(self.duration)
 
 
-# https://github.com/Rapptz/RoboDanny/blob/87772388eff2feeeabe1585efcffaffec9fe37a9/cogs/buttons.py#L89
-class Ratelimiter(commands.CooldownMapping):
-    def __init__(self, rate, per):
-        super().__init__(commands.Cooldown(rate, per, commands.BucketType.user))
+class Ratelimiter:
+    """
+    A timing mechanism to limit requests to ``rate`` per ``per`` seconds.
+    """
 
-    def __repr__(self):
-        return f'<Ratelimiter rate={self.rate} per={self.per}>'
+    def __init__(self, rate: int, per: float) -> None:
+        self.rate = rate
+        self.per = per
 
-    def __eq__(self, other):
-        if not isinstance(other, Ratelimiter):
+        self._buckets: T.Dict[T.Any, T.Tuple[float, int]] = {}
+
+    def remaining_time(self, token: T.Any) -> float:
+        """Return the time remaining until a token is able to make more requests.
+
+        If the token isn't being rate limited, then ``0.0`` is returned.
+        """
+        if not self.is_being_rate_limited(token):
+            return 0.0
+
+        last_updated, _ = self._buckets[token]
+        time_since_last_update = time.monotonic() - last_updated
+        return self.per - time_since_last_update
+
+    def is_being_rate_limited(self, token: T.Any) -> bool:
+        """Return whether a token is being ratelimited or not.
+
+        This doesn't count against the number of requests allowed within the
+        time period.
+        """
+        return self.hit(passive=True)
+
+    def hit(self, token: T.Any = True, *, passive: bool = False) -> bool:
+        """Add one to the number of performed requests, and return whether the
+        limit within the timing period has been exceeded."""
+
+        if token not in self._buckets:
+            # initial creation
+            #
+            # the initial value is 1 instead of 0 because the creation counts as
+            # a request still
+            if not passive:
+                self._buckets[token] = (time.monotonic(), 1)
             return False
+
+        last_updated, n_requests = self._buckets[token]
+        time_since_last_update = time.monotonic() - last_updated
+
+        if time_since_last_update > self.per:
+            # enough time has passed, reset tracking values
+            self._buckets[token] = (time.monotonic(), 1)
+            return False
+
+        if n_requests + 1 > self.rate:
+            # this request would exceed the amount of requests allowed within
+            # time period (being ratelimited)
+            return True
+
+        if not passive:
+            # update the last updated timestamp and number of requests
+            self._buckets[token] = (time.monotonic(), self._buckets[token][1] + 1)
+
+        return False
+
+    def __repr__(self) -> str:
+        return f"<Ratelimiter rate={self.rate} per={self.per}>"
+
+    def __eq__(self, other) -> bool:
         return self.rate == other.rate and self.per == other.per
-
-    @property
-    def rate(self):
-        return self._cooldown.rate
-
-    @property
-    def per(self):
-        return self._cooldown.per
-
-    def _bucket_key(self, v):
-        return v
-
-    def is_rate_limited(self, *args):
-        bucket = self.get_bucket(args)
-        return bucket.update_rate_limit() is not None
